cabaa72473d78b2c4a7ca86de7302dc9f1f266e1
Emanuele Buccelli <escapemanuele@gmail.com>
2019-11-08 23:45:16 +0100

    first commit

---
+++ Qod.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Coravel.Invocable;
+using Newtonsoft.Json.Linq;
+
+public class Qod : IInvocable
+{
+    public async Task Invoke()
+    {
+        var httpClient = new HttpClient();
+        var quoteJson = JObject.Parse(await httpClient.GetStringAsync("http://quotes.rest/qod.json"));
+        string quote = quoteJson["contents"]["quotes"][0].ToString();
+
+        Console.WriteLine(quote);
+
+        //return Task.CompletedTask;
+    }
+}
