7f0a89595a0c879b9bd03a95683ad8a6c093cc16
escapemanuele <escapemanuele@gmail.com>
2019-11-10 15:45:17 +0100

    add serilog

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace QuoteOfTheDay
 {
@@ -14,18 +15,37 @@ namespace QuoteOfTheDay
     {
         public static void Main(string[] args)
         {
-            IHost host = CreateHostBuilder(args).Build();
-            host.Services.UseScheduler(scheduler => {
-                scheduler
-                    .Schedule<QotdTask>()
-                    .EveryTenSeconds();
-            });
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
 
-            host.Run();
+            try
+            {
+                Log.Information("Starting up");
+                IHost host = CreateHostBuilder(args).Build();
+                host.Services.UseScheduler(scheduler => {
+                    scheduler
+                        .Schedule<QotdTask>()
+                        .EveryTenSeconds();
+                });
+
+                host.Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Application start-up failed");
+            }
+            finally 
+            {
+                Log.CloseAndFlush();
+            }
+            
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureServices(services => {
                     services.AddScheduler();
                 })
--- QotdTask.cs
+++ QotdTask.cs
@@ -1,7 +1,7 @@
-﻿using System;
-using System.Net.Http;
+﻿using System.Net.Http;
 using System.Threading.Tasks;
 using Coravel.Invocable;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
@@ -9,28 +9,29 @@ using QuoteOfTheDay;
 using QuoteOfTheDay.Context;
 using QuoteOfTheDay.Context.Repository;
 using Telegram.Bot;
-using Telegram.Bot.Types.ReplyMarkups;
 
 public class QotdTask : IInvocable
 {
+    private readonly ILogger<QotdTask> logger;
+
     private readonly BotConfiguration config;
     private readonly IRepository<Chat> db;
 
-    public QotdTask(IOptions<BotConfiguration> _config, IRepository<Chat> _db)
+    public QotdTask(ILogger<QotdTask> _logger, IOptions<BotConfiguration> _config, IRepository<Chat> _db)
     {
+        logger = _logger;
         config = _config.Value;
         db = _db;
     }
 
     public async Task Invoke()
     {
+        logger.LogInformation("QotdTask launched");
         //Retrive the quote of the day
         var httpClient = new HttpClient();
         //System.Text.Json
         var quoteJson = JObject.Parse(await httpClient.GetStringAsync("http://quotes.rest/qod.json"));
         var quote = JsonConvert.DeserializeObject<Qotd>(quoteJson["contents"]["quotes"][0].ToString());
-
-        Console.WriteLine($"Quote from {quote.Author}: {quote.Quote}");
 
         var botClient = new TelegramBotClient(config.ApiToken);
         //chatID: 96546887
@@ -41,10 +42,12 @@ public class QotdTask : IInvocable
         {
             await botClient.SendTextMessageAsync(
                     chatId: chatId,
-                    text: quote.Quote,
-                    disableNotification: false,
-                    replyMarkup: new InlineKeyboardMarkup(InlineKeyboardButton.WithUrl("Check google", "https://www.google.com"))
+                    text: $"*{quote.Author}*:\n\n{quote.Quote}",
+                    parseMode: Telegram.Bot.Types.Enums.ParseMode.Markdown,
+                    disableNotification: false
                 );
         }
+        
+        logger.LogInformation($"Sent quote to {chatsId.Count} chats");
     }
 }
